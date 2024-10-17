0a93160ea1524caa54910d2e6946dcce31ef290c
Ivan Nikolić <niksy5@gmail.com>
2024-10-17 20:15:18 +0200

    Add Node version requirement

--- plugin.py
+++ plugin.py
@@ -14,3 +14,7 @@ class LspSassPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'language-server'
     server_binary_path = os.path.join(server_directory, 'node_modules', 'some-sass-language-server', 'bin', 'some-sass-language-server')
+
+    @classmethod
+    def required_node_version(cls) -> str:
+        return '>=20'
